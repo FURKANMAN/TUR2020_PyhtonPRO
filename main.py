ad75ea2dd99efcde2347922b36d3d947b9552fb8
FURKANMAN <165496571+FURKANMAN@users.noreply.github.com>
2024-05-04 19:16:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-import random
-characters="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-password_length=int(input("Şifren kaç haneli olsun?"))
-password= ""
+import discord
+import random,os
+from discord.ext import commands
 
-for i in range(password_length):
-    character= random.choice(characters)
+intents = discord.Intents.default()
+intents.message_content = True
 
-    password += character
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-print ("işte şifren")
-print(password)
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Selamun Aleyküm Kardeşm.Ben {bot.user}, bir Discord sohbet botuyum.')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def random_mem(ctx):
+    img_list = os.listdir("images")
+    img = random.choice(img_list)
+    with open(f'images/{img}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def Kagıt_Kirliligi(ctx):
+    await ctx.send(f'Kağıt geri dönüşüm kutusuna at')
+
+
+@bot.command()
+async def Plastik_Kirliligi(ctx):
+    await ctx.send(f'Plastik geri dönüşüm kutusuna at')
+
+@bot.command()
+async def Cam_Kirliligi(ctx):
+    await ctx.send(f'Cam geri dönüşüm kutusuna at')
+
+@bot.command()
+async def Cevre_Kirliligi(ctx):
+    await ctx.send(f'Geri dönüşüm kutularını kullan ve Yenilenebilir enerji kaynağı kullan.')
+    
+
+bot.run("")
